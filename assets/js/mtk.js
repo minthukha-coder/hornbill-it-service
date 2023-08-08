66077e6acf4f5f5bde04eb5f5a957bb671101c52
Min Thu Kha <minthukha25122003@gmail.com>
2023-08-08 19:13:40 +0630

    about section finished by mtk

---
+++ assets/js/mtk.js
@@ -0,0 +1,5 @@
+let text = document.getElementById('text');
+text.innerHTML = text.innerText.split("").map(
+    (char, i) => 
+    `<span style = "transform:rotate(${i * 12.5}deg)">${char}</span>`
+).join("")
