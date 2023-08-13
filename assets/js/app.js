4cc73c65b801fff4efbf6bc215c86a4e14a66ac4
Min Thu Kha <minthukha25122003@gmail.com>
2023-08-13 15:18:09 +0630

    all finished by mtk,pl,htet

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+let text = document.getElementById('text');
+text.innerHTML = text.innerText.split("").map(
+    (char, i) => 
+    `<span style = "transform:rotate(${i * 12.5}deg)">${char}</span>`
+).join("")
+
+
+const typed = new Typed('.multiple-text',{
+    strings : ['Business','eCommerce','Personal','Technology','Entertainment'],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
