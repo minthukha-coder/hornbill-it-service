27978171ef2772aadd7ba4a1f9353d80dd175d5b
Min Thu Kha <minthukha25122003@gmail.com>
2023-09-06 09:56:28 +0630

    f

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,23 @@
 let text = document.getElementById('text');
 text.innerHTML = text.innerText.split("").map(
-    (char, i) => 
+  (char, i) =>
     `<span style = "transform:rotate(${i * 12.5}deg)">${char}</span>`
 ).join("")
 
 
-const typed = new Typed('.multiple-text',{
-    strings : ['Business','eCommerce','Personal','Technology','Entertainment'],
-    typeSpeed : 100,
-    backSpeed : 100,
-    backDelay : 1000,
-    loop : true
+const typed = new Typed('.multiple-text', {
+  strings: ['Business', 'eCommerce', 'Personal', 'Technology', 'Entertainment'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
 })
 
 var swiper = new Swiper(".mySwiper", {
-    direction: "vertical",
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev", 
-    },
-    loop : true
-  });
+  direction: "vertical",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  loop: true
+});
